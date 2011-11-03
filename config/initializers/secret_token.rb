1dd2d87728e397a902f63495b7d618e4cd68c605
Donald R. Ziesig <donald@ziesig.org>
2011-11-03 18:52:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MagicDispatcher::Application.config.secret_token = 'cf3abd526c1c51111184491841926640465522faa9bfd9314f217ffc1a64d5a61dd613bfcea036c8de5be883eb3bb883eadc0a610cf85698871d4e80198e44f4'
